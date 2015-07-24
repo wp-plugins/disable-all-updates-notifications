1ee94c004758ca07249e31bf641de88721f6b2b8
chandgiri67 <chandgiri67@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-24 05:19:23 +0000

    Easy way to disables all wordpress, themes, plugins, core updates & notifications in wordpress.
    git-svn-id: https://plugins.svn.wordpress.org/disable-all-updates-notifications/trunk@1205464 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- disable-all-wordpress-updates-notifications.php
+++ disable-all-wordpress-updates-notifications.php
@@ -4,23 +4,18 @@
 Plugin Name: Disable All Updates & Notifications
 Description: Easy way to disables all wordpress, themes, plugins, core updates & notifications in wordpress.
 Plugin URI:  http://wordpress.org/plugins/disable-all-wordpress-updates-notifications/
-Version:     1.0
+Version:     1.1
 Author:      Chandrakesh Kumar
 Author URI:  http://www.wpchandra.com/
 License:	 GPL2
 */
 
+class wpchandra_remove_all_updates_and_notifications {
 
-//Desable Update Notification
-add_action('after_setup_theme', 'remove_all_updates_and_notifications');
-
-function remove_all_updates_and_notifications() {
-
-	if (!current_user_can('update_core')) {
-		return;
-	}
+	function __construct() {	
 	add_action('init', create_function('$a', "remove_action( 'init', 'wp_version_check' );"), 2);
 	add_filter('pre_option_update_core', '__return_null');
+	add_filter( 'auto_update_core', '__return_false' );
 	add_filter('pre_site_transient_update_core', '__return_null');
 	add_filter( 'auto_update_translation', '__return_false' );
 	add_filter( 'automatic_updater_disabled', '__return_true' );
@@ -39,8 +34,21 @@ function remove_all_updates_and_notifications() {
 	remove_action('load-update-core.php','wp_update_plugins');
 	define( 'WP_AUTO_UPDATE_CORE', false );
 	define( 'AUTOMATIC_UPDATER_DISABLED', true );
+	add_filter( 'allow_dev_auto_core_updates', '__return_false' );
+	// Disable All Automatic Updates
+    add_filter( 'automatic_updater_disabled', '__return_true' );
+	// Disable Automatic Update Result Emails
+    add_filter( 'auto_core_update_send_email', '__return_false' );
 
 }
+
+
+}
+
+if ( class_exists('wpchandra_remove_all_updates_and_notifications') ) {
+	$wpchandra_remove_all_updates_and_notifications = new wpchandra_remove_all_updates_and_notifications();
+}
+
 
 
 ?>
